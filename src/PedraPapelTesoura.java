30dd4d462d8e834980535a8bf7b6a1b4a672eb70
celso.alunos <joaopedro.dancam@gmail.com>
2024-05-20 17:00:17 -0300

    Atualizacao

--- src/PedraPapelTesoura.java
+++ src/PedraPapelTesoura.java
@@ -2,35 +2,79 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class PedraPapelTesoura {
+
     public static void main(String[] args) {
         Scanner leitor = new Scanner(System.in);
         Random sorteio = new Random();
 
         System.out.println("Faça sua escolha:\n(1)pedra\n(2)papel\n(3)tesoura?");
-        int player1 = leitor.nextInt();
 
-        int computador = sorteio.nextInt(3) + 1;
-        System.out.println(computador);
 
-        if (player1 == 1 && computador == 2){
-            System.out.println("Você escolheu pedra e a máquina papel...\nlogo você perdeu");
-        } else if (player1 == 1 && computador == 3) {
-            System.out.println("Você escolheu pedra e a máquina tesoura...\nlogo você ganhou");
-        } else if (player1 == 2 && computador == 1) {
-            System.out.println("Você escolheu papel e a máquina pedra...\nlogo você ganhou");
-        } else if (player1 == 3 && computador == 2) {
-            System.out.println("Você escolheu tesoura e a máquina papel...\nlogo você ganhou");
-        } else if (player1 == 3 && computador == 1) {
-            System.out.println("Você escolheu tesoura e a máquina pedra...\nlogo você perdeu");
-        } else if (player1 == 2 && computador == 3) {
-            System.out.println("Você escolheu papel e a máquina tesoura...\nlogo você perdeu");
-        } else if (player1 == computador){
-            System.out.println("Vocês empataram, jogue novamente");
-        }
-        else {
-            System.out.println("Você escolheu um valor não inspecionado");
+        try {
+            System.out.print("Qual a sua escolha? ");
+            int player1 = leitor.nextInt();
+            int computador = sorteio.nextInt(3) + 1;
+            System.out.print("Escolha do computador: ");
+            System.out.println(computador);
+
+            System.out.println("");
+
+            System.out.println("Escolha do Jogador: " + definirNomeEscolha(player1));
+            System.out.println("Escolha do Computador " + definirNomeEscolha(computador));
+
+            System.out.println("");
+
+            determinarVencedor(player1,computador);
+        } catch (Exception erro) {
+            System.out.println("A sua escolha deve ser um NÚMERO entre 1 e 3");
         }
 
 
+
+
+    }
+
+    private static String definirNomeEscolha(int opcao){
+        String escolhaString;
+
+        if (opcao == 1) {
+            escolhaString = "Pedra";
+        } else if (opcao == 2) {
+            escolhaString = "Papel";
+        } else {
+            escolhaString = "Tesoura";
+        }
+
+        return escolhaString;
+    }
+
+
+    private static void determinarVencedor(int player1, int computador){
+
+        if (player1 == 1){
+            if (computador == 1){
+                System.out.println("O jogo empatou !!");
+            } else if (computador == 2) {
+                System.out.println("O computador venceu!!");
+            } else {
+                System.out.println("Você venceu!!");
+            }
+        } else if (player1 == 2) {
+            if (computador == 2){
+                System.out.println("O jogo empatou !!");
+            } else if (computador == 3) {
+                System.out.println("O computador venceu!!");
+            } else {
+                System.out.println("Você venceu!!");
+            }
+        } else if (player1 == 3) {
+            if (computador == 3){
+                System.out.println("O jogo empatou !!");
+            } else if (computador == 1) {
+                System.out.println("O computador venceu!!");
+            } else {
+                System.out.println("Você venceu!!");
+            }
+        }
     }
 }
